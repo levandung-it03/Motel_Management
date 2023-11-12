f1d20734894256933ae42543663cd11f3aa7e9fc
vgbao1231 <vgbao1231@gmail.com>
2023-11-12 21:19:22 +0700

    Testing First Pushing Data

--- src/com/motel_management/Main/Main.java
+++ src/com/motel_management/Main/Main.java
@@ -10,5 +10,6 @@ public class Main {
         System.out.println(a.getName());
         System.out.println(a.getUsername());
         System.out.println(a.getPassword());
+        // vogiabao gủa
     }
 }
