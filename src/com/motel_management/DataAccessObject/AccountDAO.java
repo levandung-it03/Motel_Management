bee9bed0d458647aae373f33c0f2ce51241366cd
levandungit03 <levandungit03@gmail.com>
2023-11-12 23:06:34 +0700

    Completely Constructing AccountDAO by levandung

--- src/com/motel_management/DataAccessObject/AccountDAO.java
+++ src/com/motel_management/DataAccessObject/AccountDAO.java
@@ -2,10 +2,7 @@ package com.motel_management.DataAccessObject;
 import com.motel_management.Models.Account;
 import com.motel_management.DB_interaction.DB_connection;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 
 public class AccountDAO implements DAOInterface<Account> {
@@ -16,18 +13,55 @@ public class AccountDAO implements DAOInterface<Account> {
     }
 
     @Override
-    public int delete(Account obj) {
+    public int insert(Account obj) {
+        Connection myConnection = DB_connection.getMMDBConnection();
+        try {
+            Statement st = myConnection.createStatement();
+            String query = "INSERT INTO Account VALUES ("
+                    + "\"" + obj.getUserId() + "\", "
+                    + "\"" + obj.getName() + "\", "
+                    + "\"" + obj.getUsername() + "\", "
+                    + "\"" + obj.getPassword() + "\")";
+            return st.executeUpdate(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DB_connection.closeMMDBConnection(myConnection);
+        }
         return 0;
     }
 
     @Override
-    public int insert(Account obj) {
+    public int delete(Account obj) {
+        Connection myConnection = DB_connection.getMMDBConnection();
+        try {
+            Statement st = myConnection.createStatement();
+            return st.executeUpdate("DELETE FROM Account WHERE userId=\"" + obj.getUserId() + "\"");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DB_connection.closeMMDBConnection(myConnection);
+        }
         return 0;
     }
 
     @Override
     public int update(Account obj) {
-        return 0;
+        Connection myConnection = DB_connection.getMMDBConnection();
+        try {
+            Statement st = myConnection.createStatement();
+            String query = "UPDATE Account SET "
+                    + "name=\"" + obj.getName() + "\", "
+                    + "username=\"" + obj.getUsername() + "\", "
+                    + "password=\"" + obj.getPassword() + "\" "
+                    + "WHERE (userId=\"" + obj.getUserId() +"\");";
+            System.out.println(query);
+            return st.executeUpdate(query);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            DB_connection.closeMMDBConnection(myConnection);
+        }
     }
 
     @Override
@@ -49,11 +83,41 @@ public class AccountDAO implements DAOInterface<Account> {
 
     @Override
     public ArrayList<Account> selectAll() {
+        Connection myConnection = DB_connection.getMMDBConnection();
+        try {
+            Statement st = myConnection.createStatement();
+            ArrayList<Account> result = new ArrayList<>();
+            ResultSet rs = st.executeQuery("SELECT * FROM Account");
+            while (rs.next()) {
+                result.add(new Account(rs.getString("userId"), rs.getString("name"),
+                        rs.getString("username"), rs.getString("password")));
+            }
+            return result;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DB_connection.closeMMDBConnection(myConnection);
+        }
         return null;
     }
 
     @Override
     public ArrayList<Account> selectByCondition(String condition) {
+        Connection myConnection = DB_connection.getMMDBConnection();
+        try {
+            Statement st = myConnection.createStatement();
+            ArrayList<Account> result = new ArrayList<>();
+            ResultSet rs = st.executeQuery("SELECT * FROM Account " + condition);
+            while (rs.next()) {
+                result.add(new Account(rs.getString("userId"), rs.getString("name"),
+                        rs.getString("username"), rs.getString("password")));
+            }
+            return result;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DB_connection.closeMMDBConnection(myConnection);
+        }
         return null;
     }
 }
--- src/com/motel_management/Main/Main.java
+++ src/com/motel_management/Main/Main.java
@@ -3,12 +3,16 @@ package com.motel_management.Main;
 import com.motel_management.DataAccessObject.AccountDAO;
 import com.motel_management.Models.Account;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        Account a = AccountDAO.getInstance().selectById("admin000");
-        System.out.println(a.getUserId());
-        System.out.println(a.getName());
-        System.out.println(a.getUsername());
-        System.out.println(a.getPassword());
+        AccountDAO.getInstance().update(new Account("admin001", "Dung", "levandung", "123456"));
+        AccountDAO.getInstance().selectAll().forEach(a -> {
+            System.out.println(a.getUserId());
+            System.out.println(a.getName());
+            System.out.println(a.getUsername());
+            System.out.println(a.getPassword());
+        });
     }
 }
