5925916eba71d9b8a5b06d1ffbcceba97d96bd37
levandungit03 <levandungit03@gmail.com>
2023-11-12 23:10:33 +0700

    Update queries by levandung

--- src/com/motel_management/Main/Main.java
+++ src/com/motel_management/Main/Main.java
@@ -7,12 +7,5 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        AccountDAO.getInstance().update(new Account("admin001", "Dung", "levandung", "123456"));
-        AccountDAO.getInstance().selectAll().forEach(a -> {
-            System.out.println(a.getUserId());
-            System.out.println(a.getName());
-            System.out.println(a.getUsername());
-            System.out.println(a.getPassword());
-        });
     }
 }
