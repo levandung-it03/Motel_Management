48e2881ebf77f86fc72474ddc44ffac7d813d1e6
levandungit03 <levandungit03@gmail.com>
2023-11-13 23:47:01 +0700

    Upload Views/Login Frame by levandung

--- src/com/motel_management/Views/LoginFrame.java
+++ src/com/motel_management/Views/LoginFrame.java
@@ -1,40 +1,120 @@
 package com.motel_management.Views;
 
+import com.motel_management.DataAccessObject.AccountDAO;
+import com.motel_management.Models.Account;
+
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
 
-public class LoginFrame {
-    public LoginFrame() {}
+public class LoginFrame extends JFrame {
+    int fullWith = 500, fullHeight = 350;
+    private final Color mainBackgroundColor = new Color(74, 161, 67);
+    private final Color mainTextColor = new Color(0, 0, 0);
+    private final Font labelFont = new Font("Arial", Font.PLAIN, 14);
+    private final JPanel centralPanel = new JPanel();
+    private final JLabel title = new JLabel("MOTEL");
+    private final JPanel usernamePanel = new JPanel(new BorderLayout());
+    private final JLabel usernameLabel = new JLabel("Username");
+    private final JTextField usernameField = new JTextField();
+    private final JPanel passwordPanel = new JPanel(new BorderLayout());
+    private final JLabel passwordLabel = new JLabel("Password");
+    private final JPasswordField passwordField = new JPasswordField();
+    private final JPanel buttonsPanel = new JPanel(new BorderLayout());
+    private final JButton loginBtn = new JButton("Login");
+    private final JButton resetBtn = new JButton("Reset");
 
-    public static void createLoginFrame() {
-        JFrame loginFrame = new JFrame("Motel Management - Login");
-        loginFrame.setLayout(new FlowLayout());
-        loginFrame.setSize(600, 300);
+    public LoginFrame() {
+        super("Motel Management - Login");
+    }
 
-        JPanel mainPanel = new JPanel(new BorderLayout());
-        mainPanel.setPreferredSize(new Dimension(300, 200));
+    public static void startLoginFrame() {
+        LoginFrame mainFrame = new LoginFrame();
+        mainFrame.createLoginFrame();
+        mainFrame.createListeners();
+    }
 
-        // Username
-        JPanel usernamePanel = new JPanel(new BorderLayout());
-        JLabel usernameLabel = new JLabel("Username");
-        JTextField username = new JTextField();
-        username.setPreferredSize(new Dimension(200, 100));
-        usernamePanel.add(usernameLabel, BorderLayout.WEST);
-        usernamePanel.add(username, BorderLayout.EAST);
+    public void createLoginFrame() {
+        UIManager.put("Label.font", labelFont);
+        UIManager.put("Label.foreground", mainTextColor);
 
-        // Password
-        JPanel passwordPanel = new JPanel(new BorderLayout());
-        JLabel passwordLabel = new JLabel("Password");
-        JTextField password = new JTextField();
-        password.setPreferredSize(new Dimension(200, 30));
-        passwordPanel.add(passwordLabel, BorderLayout.WEST);
-        passwordPanel.add(password, BorderLayout.EAST);
+        setLayout(new BorderLayout());
+        setSize(fullWith, fullHeight);
 
+        // -------------Central Panel-------------
+        centralPanel.setPreferredSize(new Dimension(fullWith*3/5, fullHeight));
 
-        mainPanel.add(usernamePanel, BorderLayout.NORTH);
-        mainPanel.add(passwordPanel, BorderLayout.SOUTH);
-        loginFrame.add(mainPanel);
-        loginFrame.setVisible(true);
-        loginFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        add(centralPanel, BorderLayout.CENTER);
+        // -------------Title-------------
+        title.setFont(title.getFont().deriveFont(38.0f));
+        title.setBorder(new EmptyBorder(30, 30, 10, 40));
+
+        // -------------Username (usernamePanel)-------------
+        usernameField.setPreferredSize(new Dimension(fullWith*3/5, 35));
+        usernameLabel.setFont(usernameLabel.getFont().deriveFont(14.0f));
+        usernamePanel.add(usernameLabel, BorderLayout.NORTH);
+        usernamePanel.add(usernameField, BorderLayout.CENTER);
+
+        // -------------Password (passwordPanel)-------------
+        passwordField.setPreferredSize(new Dimension(fullWith*3/5, 35));
+        passwordLabel.setFont(passwordLabel.getFont().deriveFont(14.0f));
+        passwordPanel.add(passwordLabel, BorderLayout.NORTH);
+        passwordPanel.add(passwordField, BorderLayout.CENTER);
+
+        // -------------Buttons (buttonsPanel)-------------
+        loginBtn.setPreferredSize(new Dimension(70, 30));
+        resetBtn.setPreferredSize(new Dimension(70, 30));
+        buttonsPanel.add(loginBtn, BorderLayout.WEST);
+        buttonsPanel.add(resetBtn, BorderLayout.EAST);
+        buttonsPanel.setBorder(new EmptyBorder(15, 0, 0, 0));
+        buttonsPanel.setPreferredSize(new Dimension(fullWith/2, 50));
+
+        usernamePanel.setBackground(mainBackgroundColor);
+        passwordPanel.setBackground(mainBackgroundColor);
+        buttonsPanel.setBackground(mainBackgroundColor);
+        centralPanel.setBackground(mainBackgroundColor);
+
+        centralPanel.add(title);
+        centralPanel.add(usernamePanel);
+        centralPanel.add(passwordPanel);
+        centralPanel.add(buttonsPanel);
+
+        setVisible(true);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public void createListeners() {
+        loginBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String u = usernameField.getText();
+                String p = passwordField.getText();
+                ArrayList<Account> a = AccountDAO.getInstance().selectByCondition("WHERE (username=\"" + u + "\")");
+                if (a.isEmpty()) {
+                    JOptionPane.showMessageDialog(centralPanel, "Username is not existed!");
+                } else if (!a.get(0).getPassword().equals(p)) {
+                    JOptionPane.showMessageDialog(centralPanel, "Password is not correct!");
+                } else {
+                    JOptionPane.showMessageDialog(centralPanel, "Login successfully!");
+                    setVisible(false);
+                    startMainApplication(a.get(0).getName());
+                }
+            }
+        });
+        resetBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                usernameField.setText("");
+                passwordField.setText("");
+            }
+        });
+    }
+
+    public void startMainApplication(String user) {
+        MainApplicationFrame.startMainApplicationFrame(user);
     }
 }
--- src/com/motel_management/Views/MainApplicationFrame.java
+++ src/com/motel_management/Views/MainApplicationFrame.java
@@ -1,2 +1,24 @@
-package com.motel_management.Views;public class MainAppicationFrame {
+package com.motel_management.Views;
+
+import javax.swing.*;
+
+public class MainApplicationFrame extends JFrame {
+    String user;
+    public MainApplicationFrame(String user) {
+        super("Motel Management");
+        this.user = user;
+    }
+
+    public static void startMainApplicationFrame(String user) {
+        MainApplicationFrame mainApp = new MainApplicationFrame(user);
+        mainApp.createMainFrame();
+    }
+
+    public void createMainFrame() {
+        setSize(900, 700);
+        setVisible(true);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
 }
