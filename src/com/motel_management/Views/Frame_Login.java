438ba72cee4c1fdb1bb35d67542480bdcbfc0e14
levandungit03 <levandungit03@gmail.com>
2023-11-14 09:57:42 +0700

    Update MainApp by levandung

--- src/com/motel_management/Views/Frame_Login.java
+++ src/com/motel_management/Views/Frame_Login.java
@@ -12,18 +12,14 @@ import java.util.ArrayList;
 
 public class Frame_Login extends JFrame {
     int fullWith = 500, fullHeight = 350;
-    private final Color mainBackgroundColor = new Color(74, 161, 67);
-    private final Color mainTextColor = new Color(0, 0, 0);
-    private final Font labelFont = new Font("Arial", Font.PLAIN, 14);
     private final JPanel centralPanel = new JPanel();
-    private final JLabel title = new JLabel("MOTEL");
     private final JPanel usernamePanel = new JPanel(new BorderLayout());
-    private final JLabel usernameLabel = new JLabel("Username");
-    private final JTextField usernameField = new JTextField();
     private final JPanel passwordPanel = new JPanel(new BorderLayout());
-    private final JLabel passwordLabel = new JLabel("Password");
-    private final JPasswordField passwordField = new JPasswordField();
     private final JPanel buttonsPanel = new JPanel(new BorderLayout());
+
+    private final JTextField usernameField = new JTextField();
+    private final JPasswordField passwordField = new JPasswordField();
+
     private final JButton loginBtn = new JButton("Login");
     private final JButton resetBtn = new JButton("Reset");
 
@@ -38,8 +34,12 @@ public class Frame_Login extends JFrame {
     }
 
     public void createLoginFrame() {
-        UIManager.put("Label.font", labelFont);
-        UIManager.put("Label.foreground", mainTextColor);
+        UIManager.put("Label.font", Configuration.labelFont);
+        UIManager.put("Label.foreground", Configuration.blackTextColor);
+
+        JLabel title = new JLabel("MOTEL");
+        JLabel usernameLabel = new JLabel("Username");
+        JLabel passwordLabel = new JLabel("Password");
 
         setLayout(new BorderLayout());
         setSize(fullWith, fullHeight);
@@ -72,10 +72,10 @@ public class Frame_Login extends JFrame {
         buttonsPanel.setBorder(new EmptyBorder(15, 0, 0, 0));
         buttonsPanel.setPreferredSize(new Dimension(fullWith/2, 50));
 
-        usernamePanel.setBackground(mainBackgroundColor);
-        passwordPanel.setBackground(mainBackgroundColor);
-        buttonsPanel.setBackground(mainBackgroundColor);
-        centralPanel.setBackground(mainBackgroundColor);
+        usernamePanel.setBackground(Configuration.normalGreen);
+        passwordPanel.setBackground(Configuration.normalGreen);
+        buttonsPanel.setBackground(Configuration.normalGreen);
+        centralPanel.setBackground(Configuration.normalGreen);
 
         centralPanel.add(title);
         centralPanel.add(usernamePanel);
