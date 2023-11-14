49bcb455204be2ef7335b5d5979501de713d70db
vgbao1231 <vgbao1231@gmail.com>
2023-11-14 21:46:37 +0700

    Pulling

--- src/com/motel_management/Views/MainApplication/Graphics/CentralPanelPages/StatisticPage.java
+++ src/com/motel_management/Views/MainApplication/Graphics/CentralPanelPages/StatisticPage.java
@@ -1,10 +1,7 @@
-<<<<<<< HEAD:src/com/motel_management/Views/MainApplication/Panel/CentralPanels/Statistic.java
-package com.motel_management.Views.MainApplication.Panel.CentralPanels;
+
+package com.motel_management.Views.MainApplication.Graphics.CentralPanelPages;
 import com.motel_management.Views.Configuration;
 
-=======
-package com.motel_management.Views.MainApplication.Graphics.CentralPanelPages;
->>>>>>> 1041e489a9acb4e04eff459d83ab6ec6a9f91cb1:src/com/motel_management/Views/MainApplication/Graphics/CentralPanelPages/StatisticPage.java
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
@@ -15,20 +12,28 @@ public class StatisticPage extends JPanel {
     // Constructor
     public StatisticPage(int centralPanelWidth, int centralPanelHeight) {
         // Set Layout Here
-        super(new GridLayout(2,4,10,10));
+        super(new BorderLayout());
         this.centralPanelWidth = centralPanelWidth;
         this.centralPanelHeight = centralPanelHeight;
         this.createStatisticPanel();
     }
 
     public void createStatisticPanel() {
-
+        JPanel test = new JPanel(new GridLayout(0,4,10,10));
+        JPanel test1 = new JPanel();
         for(int i=0;i<6;i++){
             JPanel a = new JPanel();
-            a.setPreferredSize(new Dimension(30,30));
+            a.setBounds(0,0,30,30);
             a.setBackground(Configuration.lightGreen);
-            add(a);
-
+            test.add(a);
         }
+        test.setBackground(Color.lightGray);
+        test.setPreferredSize(new Dimension(100,400));
+        test1.setBackground(Color.gray);
+        test.setBorder(new EmptyBorder(10,10,10,10));
+        add(test,BorderLayout.NORTH);
+        add(test1,BorderLayout.CENTER);
+
+
     }
 }
