2fdb31833177cd923bbe445b293caaef283c74d3
vgbao1231 <vgbao1231@gmail.com>
2023-11-14 23:14:44 +0700

    Updating Statistic by vogiabao

--- src/com/motel_management/Views/MainApplication/Graphics/CentralPanelPages/StatisticPage.java
+++ src/com/motel_management/Views/MainApplication/Graphics/CentralPanelPages/StatisticPage.java
@@ -1,39 +1,64 @@
 
 package com.motel_management.Views.MainApplication.Graphics.CentralPanelPages;
-import com.motel_management.Views.Configuration;
+import com.motel_management.Views.Configs;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class StatisticPage extends JPanel {
+    ArrayList<JPanel> tags = new ArrayList<JPanel>();
+    ArrayList<JLabel> labels = new ArrayList<JLabel>();
     // Constructor
     public StatisticPage() {
         // Set Layout Here
-<<<<<<< HEAD
         super(new BorderLayout());
-        this.centralPanelWidth = centralPanelWidth;
-        this.centralPanelHeight = centralPanelHeight;
-=======
-        super();
->>>>>>> a7b79d869bc86c008b792c4d09255635117dcea6
         this.createStatisticPanel();
     }
 
     public void createStatisticPanel() {
-        JPanel test = new JPanel(new GridLayout(0,4,10,10));
+        JPanel overviewPanel = new JPanel(new GridLayout(0,4,10,10));
         JPanel test1 = new JPanel();
-        for(int i=0;i<6;i++){
-            JPanel a = new JPanel();
-            a.setBounds(0,0,30,30);
-            a.setBackground(Configuration.lightGreen);
-            test.add(a);
+
+        tags.add(new JPanel(new GridLayout(2,0)));
+        tags.add(new JPanel(new GridLayout(2,0)));
+        tags.add(new JPanel(new GridLayout(2,0)));
+        tags.add(new JPanel(new GridLayout(2,0)));
+        tags.add(new JPanel(new GridLayout(2,0)));
+
+        labels.add(new JLabel("Person"));
+        labels.add(new JLabel("Room"));
+        labels.add(new JLabel("User"));
+        labels.add(new JLabel("Gura"));
+        labels.add(new JLabel("Gura"));
+
+        for (int i=0;i<tags.size();i++){
+
+
+            JLabel number = new JLabel("12");
+            number.setFont(number.getFont().deriveFont(30.0f));
+            number.setFont(number.getFont().deriveFont(Font.BOLD));
+            number.setForeground(Color.white);
+            number.setBorder(new EmptyBorder(30,10,0,10));
+
+            labels.get(i).setFont(labels.get(i).getFont().deriveFont(26.0f));
+            labels.get(i).setForeground(Color.white);
+            labels.get(i).setBorder(new EmptyBorder(0,10,10,10));
+            labels.get(i).setFont(labels.get(i).getFont().deriveFont(Font.BOLD));
+
+            tags.get(i).add(number);
+            tags.get(i).add(labels.get(i));
+            tags.get(i).setBackground(Configs.normalGreen);
+            overviewPanel.add(tags.get(i));
         }
-        test.setBackground(Color.lightGray);
-        test.setPreferredSize(new Dimension(100,400));
+
+
+        overviewPanel.setBackground(Color.lightGray);
+        overviewPanel.setPreferredSize(new Dimension(10,400));
         test1.setBackground(Color.gray);
-        test.setBorder(new EmptyBorder(10,10,10,10));
-        add(test,BorderLayout.NORTH);
+        overviewPanel.setBorder(new EmptyBorder(10,10,10,40));
+        add(overviewPanel,BorderLayout.NORTH);
         add(test1,BorderLayout.CENTER);
 
 
