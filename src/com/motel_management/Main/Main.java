a327002d0a4a3b6792d2dea72532bfef42eee94b
levandungit03 <levandungit03@gmail.com>
2023-11-14 20:18:20 +0700

    Update AccountDAO by levandung

--- src/com/motel_management/Main/Main.java
+++ src/com/motel_management/Main/Main.java
@@ -1,6 +1,8 @@
 package com.motel_management.Main;
 
+import com.motel_management.DataAccessObject.AccountDAO;
 import com.motel_management.Views.*;
+
 
 public class Main {
     public static void main(String[] args) {
--- src/com/motel_management/Views/Configuration.java
+++ src/com/motel_management/Views/Configuration.java
@@ -3,7 +3,7 @@ package com.motel_management.Views;
 import java.awt.*;
 
 public class Configuration {
-    public static final Font labelFont = new Font("Consolas", Font.PLAIN, 14);
+    public static final Font labelFont = new Font("Consolas", Font.PLAIN, 16);
 
     public static final Color blackTextColor = new Color(33, 33, 33);
     public static final Color greenTextColor = new Color(9, 65, 0);
