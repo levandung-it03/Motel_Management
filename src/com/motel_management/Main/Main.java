8f27eb87b9532ab3736270465fd3fc21e4046348
DinhToanIT2003 <trandinhtoan2003@gmail.com>
2023-11-17 09:14:25 +0700

    Pulling

--- src/com/motel_management/Main/Main.java
+++ src/com/motel_management/Main/Main.java
@@ -1,10 +1,15 @@
 package com.motel_management.Main;
 
+import com.motel_management.DataAccessObject.DAOInterface;
+import com.motel_management.DataAccessObject.ElectricRangeDAO;
+import com.motel_management.DataAccessObject.WaterRangeDAO;
 import com.motel_management.Views.*;
 
 public class Main {
     public static void main(String[] args) {
         // Starting our application.
+//        Application.createApplication();
+        WaterRangeDAO.getInstance().insert(new String[] {"W01", "Muc 1", "50", "1000"});
 
     }
 }
