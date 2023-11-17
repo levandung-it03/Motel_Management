991f108fec76f6b6ca479b4bfbc5b33f9db45458
vgbao1231 <vgbao1231@gmail.com>
2023-11-17 17:20:53 +0700

    Pulling

--- src/com/motel_management/Controllers/Controller_Contract.java
+++ src/com/motel_management/Controllers/Controller_Contract.java
@@ -2,7 +2,7 @@ package com.motel_management.Controllers;
 
 public class Controller_Contract {
     public Controller_Contract() {}
-    pubic static int addNewContract() {
-
+    public static int addNewContract() {
+        return 0;
     }
 }
