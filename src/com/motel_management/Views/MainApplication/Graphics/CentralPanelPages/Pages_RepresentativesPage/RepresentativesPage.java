dcbc9eb28d288c63a6ccd7ccd017eaf6c59153ca
LDThien999 <leepro0611@gmail.com>
2023-11-22 17:44:38 +0700

    pulling

--- src/com/motel_management/Views/MainApplication/Graphics/CentralPanelPages/Pages_RepresentativesPage/RepresentativesPage.java
+++ src/com/motel_management/Views/MainApplication/Graphics/CentralPanelPages/Pages_RepresentativesPage/RepresentativesPage.java
@@ -1,8 +1,12 @@
-package com.motel_management.Views.MainApplication.Graphics.CentralPanelPages;
+package com.motel_management.Views.MainApplication.Graphics.CentralPanelPages.Pages_RepresentativesPage;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 public class RepresentativesPage extends JPanel {
+    public static JPanel mainPage;
+    private JPanel representativesListPanel;
     // Constructor
     public RepresentativesPage() {
         // Set Layout Here
@@ -10,7 +14,11 @@ public class RepresentativesPage extends JPanel {
         this.createRepresentativesPanel();
     }
 
-    public void createRepresentativesPanel() {
-        add(new JLabel("This is Representatives!"));
+
+    public void createRepresentativesPanel(){
+        mainPage = new JPanel();
+        representativesListPanel =new RepresentativesListPage();
+        mainPage.add(representativesListPanel);
+        add(mainPage);
     }
 }
--- src/com/motel_management/Views/MainApplication/Listeners/CentralPanelPages/Listeners_Presentatives/PresentativesListeners.java
+++ src/com/motel_management/Views/MainApplication/Listeners/CentralPanelPages/Listeners_Presentatives/PresentativesListeners.java
@@ -1,2 +1,30 @@
-package com.motel_management.Views.MainApplication.Listeners.CentralPanelPages.Listeners_Presentatives;public class PresentativesListeners {
+package com.motel_management.Views.MainApplication.Listeners.CentralPanelPages.Listeners_Presentatives;
+
+import com.motel_management.DataAccessObject.PersonDAO;
+import com.motel_management.Models.PersonModel;
+import com.motel_management.Controllers.Controller_Representatives;
+import com.motel_management.Views.MainApplication.Graphics.CentralPanelPages.Pages_RepresentativesPage.RepresentativesListPage;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class PresentativesListeners {
+    public static MouseAdapter getInformationByClick(JTable table) {
+        return new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                int clickedRow = table.rowAtPoint(e.getPoint());
+                int clickedColumn = table.columnAtPoint(e.getPoint());
+
+                // Delete Button Clicked
+                if (clickedColumn == table.getColumnCount() - 1) {
+                    PersonModel res = PersonDAO.getInstance().selectById(String.valueOf(table.getValueAt(clickedRow,0)));
+                    Controller_Representatives.showInformationById(res);
+                }
+            }
+        };
+    }
 }
