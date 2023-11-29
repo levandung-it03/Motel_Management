3b795fbc82a526d777a7a7ab2b074d15fad9cd94
levandungit03 <levandungit03@gmail.com>
2023-11-29 23:22:21 +0700

    Add ChangePasswordFrame And Change LoginFrame by levandung

--- src/com/motel_management/Controllers/Controller_Login.java
+++ src/com/motel_management/Controllers/Controller_Login.java
@@ -11,13 +11,15 @@ public class Controller_Login {
     public Controller_Login() { super(); }
 
     public static String validate(String username, String password) {
+        if (username.isEmpty() || password.isEmpty())
+            return null;
         ArrayList<AccountModel> account = AccountDAO.getInstance().selectByCondition("WHERE (username=\"" + username + "\")");
 
 //        String bcryptHashString = BCrypt.withDefaults().hashToString(10, password.toCharArray());
         BCrypt.Result result = BCrypt.verifyer().verify(password.toCharArray(), account.get(0).getPassword());
-        if (!account.isEmpty() && result.verified)
-            return account.get(0).getName();
-        else
+        if (account.isEmpty() || !result.verified)
             return null;
+
+        return account.get(0).getName();
     }
 }
