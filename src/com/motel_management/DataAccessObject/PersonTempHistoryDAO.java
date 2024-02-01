1934e7ad9f6189302bf1cbbd47b6d1c3d46eb41d
levandung-it03 <levandung.it03@gmail.com>
2024-02-01 12:39:41 +0700

    Update Add Contract Controller by levandung

--- src/com/motel_management/DataAccessObject/PersonTempHistoryDAO.java
+++ src/com/motel_management/DataAccessObject/PersonTempHistoryDAO.java
@@ -3,9 +3,8 @@ package com.motel_management.DataAccessObject;
 public class PersonTempHistoryDAO extends PersonStereoTypeDAO {
 
     // Constructor
-    public PersonTempHistoryDAO() { super("PersonTempHistory"); }
+    public PersonTempHistoryDAO() { super("Person_Temp_History"); }
 
     public static PersonTempHistoryDAO getInstance() { return new PersonTempHistoryDAO(); }
-
 
 }
