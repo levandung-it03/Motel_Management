a99f2eae282147baded35f18049184b6b2876f02
levandung-it03 <levandung.it03@gmail.com>
2024-02-21 10:09:54 +0700

    Change Needs by levandung

--- src/com/motel_management/Controllers/Controller_Contract.java
+++ src/com/motel_management/Controllers/Controller_Contract.java
@@ -65,7 +65,7 @@ public class Controller_Contract {
                 result.put("message", "There was a Contract added in the last 24h. Please wait to add new Contract!");
                 return result;
             } else {
-                // Clear the last updated Person history to save new (if there are the old Person with new info).
+                // Clear the last updated Person history to save new (if there are the old Persons with new info).
                 PersonTempHistoryDAO.getInstance().delete(data.get("identifier"));
             }
         } catch (NullPointerException ignored) {}
